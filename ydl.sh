2905ce158dfe8ff6d50831cb092c1ed3b5cee9ca
Marko Marsenic <mark0@Markos-MacBook-Pro.local>
2025-02-08 16:10:18 +0000

    simplify

--- ydl.sh
+++ ydl.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
+# Check if a URL is provided as an argument
 if [ -z "$1" ]; then
   echo "Usage: $0 <YouTube URL>"
   exit 1
 fi
 
-youtube-dl "$1"
+# Run youtube-dl and capture the output
+yt-dlp --restrict-filenames --extract-audio --audio-format mp3 "$1"
 
-# Get the name of the most recently created file in the directory
-downloaded_file=$(ls -t | head -n 1)
-
-file_name="${downloaded_file%.*}"  # Remove extension
-extension="${downloaded_file##*.}" # Get extension
-
-# Convert the file name to lowercase and replace spaces and special characters with dashes
-new_file_name=$(echo "$file_name" | tr '[:upper:]' '[:lower:]' | tr -s ' ' '-' | tr -cd '[:alnum:]-')
-
-new_file_name="${new_file_name}.${extension}"
-
-mv "$downloaded_file" ~/Documents/mp3s/"$new_file_name"
-
-echo "Downloaded and renamed '$downloaded_file' to '$new_file_name'"
+mv ./*.mp3 ~/documents/mp3s/
